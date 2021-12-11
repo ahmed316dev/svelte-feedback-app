f5ef813b9b8db7b4559e9a49a939fb7172705e04
influencia316 <ahmed.majdi316@gmail.com>
2021-12-11 11:17:32 +0200

    Trying out Svelte for the first time

---
+++ src/components/Card.svelte
@@ -0,0 +1,14 @@
+<div class="cards">
+    <slot></slot>
+</div>
+
+<style>
+    .cards{
+        background-color: white;
+        color: #333;
+        border-radius: 15px;
+        padding: 40px 50px;
+        margin: 20px 0;
+        position: relative;
+    }
+</style>
